5fc4ed46a9eab2014405e5a124fb13013c2a1cb3
zeroxys <miguelzavalac@gmail.com>
2018-01-31 18:39:20 -0600

    adding react vector icons

--- android/app/src/main/java/com/gas/MainApplication.java
+++ android/app/src/main/java/com/gas/MainApplication.java
@@ -8,6 +8,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import com.airbnb.android.react.maps.MapsPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
               new MainReactPackage(),
+              new VectorIconsPackage(),
               new MapsPackage()
       );
     }
